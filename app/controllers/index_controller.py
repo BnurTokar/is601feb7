aeb73001a5ff75bdfd78b04485fa301315080692
beyzanurtokar <bt268@njit.edu>
2022-02-12 21:46:51 -0500

    Updated my name to hello 3

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Beyzanur Tokar Hello"
+        name = "Beyzanur Tokar Hello 1"
         return render_template('index.html', name=name)
